c0c54af2bf9d977689b1295ae49298d6100a02a9
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2023-11-18 21:10:00 +0600

    Odd_even_number

---
+++ Odd_even_number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Odd_even_number
+ */
+public class Odd_even_number {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + "even number");
+        } else {
+            System.out.println(number + "even number");
+        }
+    }
+}
